21582f9622c0896e92af4cdd69b01fd91c3cd389
thoscc <thomas.chaine78@gmail.com>
2023-04-14 14:06:39 +0200

    W2S3:exercise-setting-up-the-html-document

--- src/Nav.js
+++ src/Nav.js
@@ -1,5 +1,39 @@
+import logo from "./assets/Logo.svg"
+import "./Nav.css"
+
 export default function Nav(props) {
     return (
-        <nav>{props.children}</nav>
+        <nav>
+            <ul>
+                <li>
+                    <img src={logo} alt="Little Lemon Logo"/>
+                </li>
+
+                <li>
+                    <a href="#home">Home</a>
+                </li>
+
+                <li>
+                    <a href="#about">About</a>
+                </li>
+
+                <li>
+                    <a href="#menu">Menu</a>
+                </li>
+
+                <li>
+                    <a href="#reservations">Reservations</a>
+                </li>
+
+                <li>
+                    <a href="#orderonline">Order Online</a>
+                </li>
+
+                <li>
+                    <a href="#login">Login</a>
+                </li>
+
+            </ul>
+        </nav>
     );
 }
